be3f3976041645c9b91ce554cb516ca412e92973
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-09-02 11:26:48 -0500

    converted adjective to two nouns

--- src/App.js
+++ src/App.js
@@ -5,34 +5,31 @@ import randomWords from 'random-words'
 
 const App = () =>{ 
 
-  const [adjective, setAdjective] = useState('');
-  const [noun, setNoun] = useState('');
+  const [noun1, setNoun1] = useState('');
+  const [noun2, setNoun2] = useState('');
   const [isLoading,setLoading] = useState(false)
 
   useEffect(() => {
     const getWords = async () => {
       
-      let nounFound = false
-      let adjFound = false
-      let count = 0;
+      let nounFound = 0
         try {
             setLoading(true)
-            while(nounFound === false || adjFound === false){
+            while(nounFound < 2){
               let random = randomWords()
               const word = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${random}?key=01c77953-d1ac-4bda-b229-71633cd93e15`)
               console.log(word.data[0].fl)
               let wordType = word.data[0].fl
-              if(wordType === 'adjective') {
-                adjFound = true;
-                setAdjective(random)
-              }
               if(wordType === 'noun'){
-                nounFound = true;
-                setNoun(random)
+                if(nounFound == 0){
+                  setNoun1(random)
+                }
+                if(nounFound == 1){
+                  setNoun2(random)
+                }
+                nounFound++;
               }
-              count++;
             }
-          console.log(`${count} requests`)
           setLoading(false)
             
         } catch (error) {
@@ -44,7 +41,7 @@ const App = () =>{
     return (
       <div style = {{maxWidth: '1000px', margin: '50px auto', boxShadow: '0px 0px 4px #aaa', padding: '20px', borderRadius: '8px'}}>
         <h1 style={{textAlign: 'center'}}>Random Idea Generator</h1>
-        {isLoading ? <h2>LOADING</h2> : <h2 style = {{textAlign: 'center'}}>{adjective} {noun}</h2>}
+        {isLoading ? <h2>LOADING</h2> : <h2 style = {{textAlign: 'center'}}>{noun1} {noun2}</h2>}
       </div>
     )
 }
